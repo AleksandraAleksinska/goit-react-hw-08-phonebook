3f6ffdc0cda8a563a6e50a8395246094bf058dfd
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-07-23 19:24:59 +0200

    css part 3 and alerts added

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -5,7 +5,6 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const fetchContacts = createAsyncThunk('contacts/fetchAll', async (_, thunkAPI) => {
     try {
         const response = await axios.get('/contacts');
-        console.log(response.data)
         return response.data;
     } catch (error) {
         return thunkAPI.rejectWithValue(error.message)
